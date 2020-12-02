6b418ad7adb0d81291af0a5f9f027dff8fe7e3fe
clysss <55925546+clysss@users.noreply.github.com>
2020-12-02 10:24:09 +0100

    Update WebSocketStreamClient.h
    modified flush, stop and connect (IP..) declarations/code to meet client.h

--- WebSocketStreamClient.h
+++ WebSocketStreamClient.h
@@ -2,105 +2,102 @@
 #define __WEBSOCKETSTREAMCLIENT_H_
 
 #include <WiFiClientSecure.h>
-#include "WebSocketClient250.h"
+#include <WebSocketClient.h>
 #include <Client.h>
 
 class WebSocketStreamClient : public Client
 {
-public:
-  WebSocketStreamClient(WebSocketClient250 &webSocketClient, const char *path)
-  {
-    _webSocketClient = &webSocketClient;
-    _path = path;
-  }
+  public:
 
-  int connect(CONST IPAddress &ip, uint16_t port)
-  {
-    _webSocketClient->begin(_path);
-    return 1;
-  }
+    WebSocketStreamClient(WebSocketClient &webSocketClient, const char *path)
+    {
+      _webSocketClient = &webSocketClient; 
+      _path = path;
+    }
 
-  int connect(const char *host, uint16_t port)
-  {
-    _webSocketClient->begin(_path);
-    return 1;
-  }
+    int connect(IPAddress ip, uint16_t port)
+    {
+      _webSocketClient->begin(_path);
+      return 1;
+    }
 
-  size_t write(uint8_t b)
-  {
-    if (!connected())
-      return -1;
-    return write(&b, 1);
-  }
+    int connect(const char *host, uint16_t port)
+    {
+      _webSocketClient->begin(_path);
+      return 1;
+    }
 
-  size_t write(const uint8_t *buf, size_t size)
-  {
-    if (!connected())
-      return -1;
-    _webSocketClient->beginMessage(TYPE_BINARY);
-    _webSocketClient->write(buf, size);
-    _webSocketClient->endMessage();
-    return size;
-  }
+    size_t write(uint8_t b)
+    {
+      if (!connected())
+        return -1;
+      return write(&b, 1);
+    }
 
-  int available()
-  {
-    if (!connected())
-      return 0;
-    if (_webSocketClient->available() == 0)
-      _webSocketClient->parseMessage();
-    return _webSocketClient->available();
-  }
+    size_t write(const uint8_t *buf, size_t size)
+    {
+      if (!connected())
+        return -1;
+      _webSocketClient->beginMessage(TYPE_BINARY);
+      _webSocketClient->write(buf, size);
+      _webSocketClient->endMessage();
+      return size;
+    }
 
-  int read()
-  {
-    if (!connected() || !available())
-      return -1;
-    return _webSocketClient->read();
-  }
+    int available()
+    {
+      if (!connected())
+        return 0;
+      if (_webSocketClient->available() == 0)
+        _webSocketClient->parseMessage();
+      return _webSocketClient->available();
+    }
 
-  int read(uint8_t *buf, size_t size)
-  {
-    if (!connected() || !available())
-      return -1;
-    return _webSocketClient->read(buf, size);
-  }
+    int read()
+    {
+      if (!connected() || !available())
+        return -1;
+      return _webSocketClient->read();
+    }
 
-  int peek()
-  {
-    if (!connected() || !available())
-      return -1;
-    return _webSocketClient->peek();
-  }
+    int read(uint8_t *buf, size_t size)
+    {
+      if (!connected() || !available())
+        return -1;
+      return _webSocketClient->read(buf, size);
+    }
 
-  bool flush(unsigned int maxWaitMs = 0) {
-    if (!connected())
-      return false;
-    _webSocketClient->flush();
-    return true;
-  }
+    int peek()
+    {
+      if (!connected() || !available())
+        return -1;
+      return _webSocketClient->peek();
+    }
 
-  bool stop(unsigned int maxWaitMs = 0)
-  {
-    if (!connected())
-      return false;
-    _webSocketClient->stop();
-    return true;
-  }
+    void flush() {
+      if (connected())
+        _webSocketClient->flush();
+    }
 
-  uint8_t connected()
-  {
-    return _webSocketClient->connected();
-  }
+    void stop()
+    {
+      if (connected())
+        _webSocketClient->stop();
+    }
 
-  operator bool()
-  {
-    return _webSocketClient != NULL;
-  }
+    uint8_t connected()
+    {
+      return _webSocketClient->connected();
+    }
 
-private:
-  WebSocketClient250 *_webSocketClient;
-  const char *_path;
+    operator bool()
+    {
+      return _webSocketClient != NULL;
+    }
+
+  private:
+    WebSocketClient *_webSocketClient; 
+    const char *_path;
 };
 
 #endif
